ff60a64305152c007b5aa2d297cb9e305f3dcaa4
carson ross <carson.yaumy@gmail.com>
2017-09-19 17:46:05 +0800

    ok

--- app.js
+++ app.js
@@ -88,3 +88,37 @@ app.post("/webhook", function (req, res) {
       }
     });
   }
+
+  function processMessage(event) {
+    if (!event.message.is_echo) {
+      var message = event.message;
+      var senderId = event.sender.id;
+  
+      console.log("Received message from senderId: " + senderId);
+      console.log("Message is: " + JSON.stringify(message));
+  
+      // You may get a text or attachment but not both
+      if (message.text) {
+        var formattedMsg = message.text.toLowerCase().trim();
+  
+        // If we receive a text message, check to see if it matches any special
+        // keywords and send back the corresponding movie detail.
+        // Otherwise, search for new movie.
+        switch (formattedMsg) {
+          case "plot":
+          case "date":
+          case "runtime":
+          case "director":
+          case "cast":
+          case "rating":
+            sendMessage(senderId, {text: "Sorry, I don't understand your request."});
+            break;
+  
+          default:
+            sendMessage(senderId, {text: "on99"});
+        }
+      } else if (message.attachments) {
+        sendMessage(senderId, {text: "Sorry, I don't understand your request."});
+      }
+    }
+  }
